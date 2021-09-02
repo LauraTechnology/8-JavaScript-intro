203af82f015ed7c74ec42c479a038d874d821480
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-02 13:02:58 +0300

    kaip veikia const; console.log

--- js/01-variables.js
+++ js/01-variables.js
@@ -9,4 +9,28 @@ console.log('Man yra 28 metai');
 console.log("Man yra 28 metai");
 
 const vardas = 'Laura';
-console.log(va)
+console.log(vardas);
+
+const pirmasSkaicius = 7;
+const antrasSkaicius = 77;
+const suma = pirmasSkaicius + antrasSkaicius;
+
+console.log(pirmasSkaicius);
+console.log(antrasSkaicius);
+console.log(suma);
+
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+const mathText = pirmasSkaicius + '+' + antrasSkaicius + '=' + suma;
+console.log(mathText);
+
+/* + ir = zenklai kabutese, nes spausdina skirtingu tipu reiksmes*/
+
+console.log('-------------------');
+
+const name = 'Petras';
+const age = 99;
+
+//Petras age is 99.
+const petrasAge = name + ' age is ' + age + '.';
+console.log(petrasAge)
