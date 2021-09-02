eb3c7a10a3219ca1904b01871c48745d6804deef
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-02 15:57:26 +0300

    ND 1 uzduotis

---
+++ js/01-kintamuju-inicijavimas-ND.js
@@ -0,0 +1,20 @@
+// 1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+var pirmadienis = 1;
+console.log(pirmadienis);
+
+var antradienis = 2;
+console.log(a)
+
+var treciadienis = 3;
+console.log(treciadienis);
+
+
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
