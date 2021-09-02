a4573ab97481c19cd3166be568d42741b5938d12
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-02 12:29:37 +0300

    const reikšmė visada constant

---
+++ js/01-variables.js
@@ -0,0 +1,12 @@
+console.log(44444);
+console.log(3.252525);
+console.log(3,252525);
+console.log("Labas");
+console.log('rytas');
+console.log('mano', 'vardas', 'yra');
+console.log('Mano vardas yra');
+console.log('Man yra 28 metai');
+console.log("Man yra 28 metai");
+
+const vardas = 'Laura';
+console.log(va)
