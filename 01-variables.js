e4ab8ed63c04271b360353f0488e0f1aece4ef50
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-02 12:30:21 +0300

    perkelta i js folder

--- 01-variables.js
+++
@@ -1,9 +0,0 @@
-console.log(44444);
-console.log(3.252525);
-console.log(3,252525);
-console.log("Labas");
-console.log('rytas');
-console.log('mano', 'vardas', 'yra');
-console.log('Mano vardas yra');
-console.log('Man yra 28 metai');
-console.log("Man yra 28 metai");
