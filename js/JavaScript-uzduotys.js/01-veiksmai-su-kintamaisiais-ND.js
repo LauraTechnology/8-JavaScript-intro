126544598c991e81a7485b8d8ef882651169b309
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-03 21:14:07 +0300

    Sukurtas atskiras folderis uzduotims

---
+++ js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
@@ -0,0 +1,18 @@
+//1.	Susumuoti visus skaičiaus tipo kintamuosius
+//a.	Rezultatą išvesti į console
+
+
+
+//2.	Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+//a.	Rezultatą išvesti į console
+
+
+
+//3.	Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+//a.	1-2+3-4+5
+//b.	Rezultatą išvesti į console
+
+
+
+//4.	Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+
