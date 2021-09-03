de46d4e4bf6bfafd6a0d15c06a9465bc7390d2ee
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-03 22:48:25 +0300

    In Progress-veiksmai su kintamaisiais

--- js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
+++ js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
@@ -1,16 +1,39 @@
 //1.	Susumuoti visus skaičiaus tipo kintamuosius
 //a.	Rezultatą išvesti į console
+let pirmadienis = 1;
+
+let antradienis = 2;
+
+let treciadienis = 3;
+
+let skaiciuTipoKintamujuSuma = pirmadienis + antradienis + treciadienis;
+console.log(skaiciuTipoKintamujuSuma);
 
 
 
-//2.	Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+//2.	Sujungti visus teksto tipo kintamuosius taip, 
+//      jog tarp jų būtų sudarytas tarpas
 //a.	Rezultatą išvesti į console
+
+let a = 'zalia';
+let b = 'aukstai';
+let c = 'grazu';
+
+let sujungtiTekstoTipoKintamieji = 'zalia'+' '+'aukstai'+' '+'grazu';
+console.log(sujungtiTekstoTipoKintamieji);
+
 
 
 
 //3.	Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
 //a.	1-2+3-4+5
 //b.	Rezultatą išvesti į console
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
 
 
 
