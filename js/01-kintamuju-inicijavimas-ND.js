d5f3d8a0aa6d88d79e9c35fd6828decd72949c34
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-03 21:04:32 +0300

    Done-kintamuju inicijavimas

--- js/01-kintamuju-inicijavimas-ND.js
+++ js/01-kintamuju-inicijavimas-ND.js
@@ -4,17 +4,49 @@ let pirmadienis = 1;
 console.log(pirmadienis);
 
 let antradienis = 2;
-console.log(a)
+console.log(antradienis);
 
 let treciadienis = 3;
 console.log(treciadienis);
 
+console.log('-------------------');
 
 //Sukurti 3 kintamuosius su teksto tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
 
-//Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+let a = 'zalia';
+console.log(a);
+
+let b = 'aukstai';
+console.log(b);
+
+let c = 'grazu';
+console.log(c);
+
+console.log('-------------------');
+
+//Sukurti 3 sąrašo tipo (array) kintamuosius su penkiomis skaičių tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
 
-//Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+let menesioDienos = [15, 23, 6, 12, 11];
+console.log(menesioDienos);
+
+let savaitesDienos = [1, 2, 3, 4, 5];
+console.log(savaitesDienos);
+
+let darboDienos = [4, 8, 10, 12, 14];
+console.log(darboDienos);
+
+console.log('-------------------');
+
+//Sukurti 3 sąrašo tipo (array) kintamuosius su penkiomis teksto tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
+
+let savaite = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
+console.log(savaite);
+
+let menesiai = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze'];
+console.log(menesiai);
+
+let sezonai = ['vasara', 'pavasaris', 'ziema', 'ruduo', 'atostogos'];
+console.log(sezonai);
