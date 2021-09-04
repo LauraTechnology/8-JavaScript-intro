505b39d0331e8c5fa472fe4bed03fec0336f84dd
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-04 20:15:04 +0300

    Finished

--- js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
+++ js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
@@ -32,16 +32,34 @@ console.log('-------------------');
 //b.	Rezultatą išvesti į console
 
 let menesioDatos = [15, 23, 6, 12, 11];
-let valueMenesioDienos = 15 + 23 + 6 + 12 + 11;
+let valueMenesioDienos = 15 - 23 + 6 - 12 + 11;
 console.log(valueMenesioDienos);
 
 let savaitesNumeriai = [1, 2, 3, 4, 5];
+let valueSavNum = 1 - 2 + 3 - 4 + 5;
+console.log(valueSavNum);
 
 
 let lyginesDienos = [4, 8, 10, 12, 14]
+let valueLygDien = 4 - 8 + 10 - 12 + 14;
+console.log(valueLygDien)
 
 console.log('-------------------');
 
 
-//4.	Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+//4. Sujungti sąrašų vertes, kurių tipas yra tekstai, 
+//   nuo sąrašo galo iki pradžios taip, 
+//   jog tarp jų būtų kablelis ir tarpas
+
+let dienosDarbui = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
+let valueDienosDarbui = 'pirmadienis'+','+' '+'antradienis'+','+' '+'treciadienis'+','+' '+'ketvirtadienis'+','+' '+'penktadienis';
+
+console.log(valueDienosDarbui);
+
+
+let pirmiPenkiMenesiai = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze'];
+
+
+let metuSezonai = ['vasara', 'pavasaris', 'ziema', 'ruduo', 'atostogos'];
+
 
