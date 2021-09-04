be7a4e880eed805161c939bb555f4b3de39ce463
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-04 18:28:43 +0300

    InProgress

--- js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
+++ js/JavaScript-uzduotys.js/01-veiksmai-su-kintamaisiais-ND.js
@@ -1,40 +1,46 @@
 //1.	Susumuoti visus skaičiaus tipo kintamuosius
 //a.	Rezultatą išvesti į console
-let pirmadienis = 1;
+let pirmas = 1;
 
-let antradienis = 2;
+let antras = 2;
 
-let treciadienis = 3;
+let trecias = 3;
 
-let skaiciuTipoKintamujuSuma = pirmadienis + antradienis + treciadienis;
+let skaiciuTipoKintamujuSuma = pirmas + antras + trecias;
 console.log(skaiciuTipoKintamujuSuma);
 
+console.log('-------------------');
 
 
 //2.	Sujungti visus teksto tipo kintamuosius taip, 
 //      jog tarp jų būtų sudarytas tarpas
 //a.	Rezultatą išvesti į console
 
-let a = 'zalia';
-let b = 'aukstai';
-let c = 'grazu';
+let spalva = 'zalia';
+let ugis = 'aukstai';
+let isvaizda = 'grazu';
 
 let sujungtiTekstoTipoKintamieji = 'zalia'+' '+'aukstai'+' '+'grazu';
 console.log(sujungtiTekstoTipoKintamieji);
 
 
+console.log('-------------------');
 
 
-//3.	Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+//3.	Apskaičiuoti vertę (value) iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
 //a.	1-2+3-4+5
 //b.	Rezultatą išvesti į console
 
-let a = 1;
-let b = 2;
-let c = 3;
-let d = 4;
-let e = 5;
+let menesioDatos = [15, 23, 6, 12, 11];
+let valueMenesioDienos = 15 + 23 + 6 + 12 + 11;
+console.log(valueMenesioDienos);
 
+let savaitesNumeriai = [1, 2, 3, 4, 5];
+
+
+let lyginesDienos = [4, 8, 10, 12, 14]
+
+console.log('-------------------');
 
 
 //4.	Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
