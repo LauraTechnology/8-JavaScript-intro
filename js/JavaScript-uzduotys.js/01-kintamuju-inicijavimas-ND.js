c1c5d355aaebf3b63c8916d6c1973de9440a7566
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-05 17:36:37 +0300

    pakeitimas let --> const

--- js/JavaScript-uzduotys.js/01-kintamuju-inicijavimas-ND.js
+++ js/JavaScript-uzduotys.js/01-kintamuju-inicijavimas-ND.js
@@ -1,12 +1,12 @@
 // 1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
 // Po kiekvieno jų inicijavimo, išvesti į console
-let pirmadienis = 1;
+const pirmadienis = 1;
 console.log(pirmadienis);
 
-let antradienis = 2;
+const antradienis = 2;
 console.log(antradienis);
 
-let treciadienis = 3;
+const treciadienis = 3;
 console.log(treciadienis);
 
 console.log('-------------------');
@@ -14,13 +14,13 @@ console.log('-------------------');
 //Sukurti 3 kintamuosius su teksto tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
 
-let a = 'zalia';
+const a = 'zalia';
 console.log(a);
 
-let b = 'aukstai';
+const b = 'aukstai';
 console.log(b);
 
-let c = 'grazu';
+const c = 'grazu';
 console.log(c);
 
 console.log('-------------------');
@@ -28,13 +28,13 @@ console.log('-------------------');
 //Sukurti 3 sąrašo tipo (array) kintamuosius su penkiomis skaičių tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
 
-let menesioDienos = [15, 23, 6, 12, 11];
+const menesioDienos = [15, 23, 6, 12, 11];
 console.log(menesioDienos);
 
-let savaitesDienos = [1, 2, 3, 4, 5];
+const savaitesDienos = [1, 2, 3, 4, 5];
 console.log(savaitesDienos);
 
-let darboDienos = [4, 8, 10, 12, 14];
+const darboDienos = [4, 8, 10, 12, 14];
 console.log(darboDienos);
 
 console.log('-------------------');
@@ -42,11 +42,11 @@ console.log('-------------------');
 //Sukurti 3 sąrašo tipo (array) kintamuosius su penkiomis teksto tipo reikšmėmis
 //Po kiekvieno jų inicijavimo, išvesti į console
 
-let savaite = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
+const savaite = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
 console.log(savaite);
 
-let menesiai = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze'];
+const menesiai = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze'];
 console.log(menesiai);
 
-let sezonai = ['vasara', 'pavasaris', 'ziema', 'ruduo', 'atostogos'];
+const sezonai = ['vasara', 'pavasaris', 'ziema', 'ruduo', 'atostogos'];
 console.log(sezonai);
