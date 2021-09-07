3de7a6318c27fd00c65ca42f635a7c92bffe76bc
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-07 15:04:16 +0300

    -

--- js/JavaScript-uzduotys.js/01-paskutine-raide.js
+++ js/JavaScript-uzduotys.js/01-paskutine-raide.js
@@ -1,12 +1,16 @@
+console.log('-------------------');
+
 const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
 console.log(abc);
 
-//Is mums zinomos 6 raidziu ebceles paskutine yra "f".
+//Is mums zinomos 6 raidziu abeceles paskutine yra "f".
 const abcIlgis = abc.length;
-console.log(abcIlgis);
 
 
-//const paskutinesRaidesPozicija = abcIlgis - 1;
-//const paskutineRaide = abc[paskutinesRaidesPozicija];
 
-//console.log(paskutineRaide);
+const paskutinesRaidesPozicija = abcIlgis - 1;
+const paskutineRaide = abc[paskutinesRaidesPozicija];
+
+console.log(paskutineRaide);
+
+console.log('-------------------');
