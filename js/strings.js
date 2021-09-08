b4016140d17206250d6a36ebfbf8f1ac83b8e426
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-08 19:58:44 +0300

    Visos kombinacijos darbui su tekstu/string

--- js/strings.js
+++ js/strings.js
@@ -33,12 +33,38 @@ console.log(maxKabutes1);
 const maxKabutes2 = "Max saugumas dviguba (\") ir vienguba (\') kabutes.";
 console.log(maxKabutes2);
 
-const navHTML = '<nav>\
-                    <a href="#">Link</a>\
-                    <a href="#">Link</a>\
-                    <a href="#">Link</a>\
-                    <a href="#">Link</a>\ 
-                </nav>';
+const navHTML = `<nav>\
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a> 
+                </nav>`;
 console.log(navHTML);
 
 console.log('-------------');
+
+// Mano vardas: Petras.
+const vardas = 'Maryte';
+const petrasIntro = 'Mano vardas: ' + vardas + '.';
+console.log(petrasIntro);
+
+const petrasBacktick = `Mano vardas: ${vardas}.`;
+console.log(petrasBacktick);
+
+// Naujausias pazimys: 2.
+const marks = [10, 5, 8, 7, 2, 6, 8, 10];
+const markSentence0 = `Pirmas pazimys: ${marks[0]}.`;
+console.log(markSentence0);
+const markSentence1 = `Antras pazimys: ${marks[1]}.`;
+console.log(markSentence1);
+const markSentence = `Naujausias pazimys: ${marks[marks.length - 1]}.`;
+console.log(markSentence);
+
+
+const navHTML2 = `<nav>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </nav>`;
+console.log(navHTML2);
