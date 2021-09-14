b0c55461eaa480755283190e19325cd0a71ea06d
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-14 12:06:58 +0300

    if-else lokia su ! operatoriumi

--- js/03-if.js
+++ js/03-if.js
@@ -12,3 +12,24 @@ palyginimo operatoriai:
 -nenaudotini: ==, !=, (nes nera tikrinamas reiksmiu tipas (ar skaitinis(123) ar tekstinis(abc))
 
 */
+
+const cookiesAgreed = false;
+
+if (cookiesAgreed === false) {
+    console.log('Ismetame cookies sutikimo bloka... 1');
+}
+
+if (!cookiesAgreed) {
+    console.log('Ismetame cookies sutikimo bloka... 2');
+}
+
+console.log('-------------------');
+
+const (userLoggedIn) = true;
+
+if (userLoggedIn) {
+    console.log('Log out');
+} else {
+    console.log('Log in');
+}
+
