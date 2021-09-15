b43f73e397f02d41056b98c7cacd2c6ebb8af12d
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-15 13:41:29 +0300

    Proceso logika su IF ELSE

--- js/04-switch.js
+++ js/04-switch.js
@@ -6,7 +6,7 @@ switch, case, break
 
 */
 
-const animal = 'dog';
+const animal = 'cat';
 
 switch (animal) {
     case 'dog':
@@ -29,3 +29,62 @@ if (animal === 'dog') {
 } else {
     console.log('Unknown animal');
 }
+
+console.log('----------');
+
+// Arbatos paruosimo logika
+
+const process = 1;
+
+if (process === 1) {
+    console.log('paimti puodeli');
+} else if (process === 2) {
+    console.log('isisdeti arbatos');
+} else if (process === 3) {
+    console.log('isideti saldikili');
+} else if (process === 4) {
+    console.log('ipilti vandens');
+} else if (process === 5) {
+    console.log('ismiasyti');
+} else if (process === 6) {
+    console.log('gerti');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//switch (procesas) {
+//    case 1:
+//}
