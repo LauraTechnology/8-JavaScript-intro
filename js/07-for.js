0ffed1ea55794a6cdfe210473a7f213a9f16a2a5
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-17 12:46:23 +0300

    for loop - ejimas per sarasa

--- js/07-for.js
+++ js/07-for.js
@@ -6,3 +6,12 @@ for (let i=3; i <=7; i++) {
 }
 
 console.log('ciklo pabaiga');
+
+console.log('-----------');
+
+// Ejimas per sarasa:
+const marks = [5, 7, 10, 4, 2, 10, 8, 9, 1, 3];
+
+for (let i = 0; i < marks.length; i++) {
+    console.log(marks[i]);
+}
