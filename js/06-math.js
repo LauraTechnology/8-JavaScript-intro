a379dde818a106a6024ae1073b2705bced98bd6e
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-19 11:26:55 +0300

    Matematiniai operatoriai #6

---
+++ js/06-math.js
@@ -0,0 +1,17 @@
+const marks2 = [2, 4, 6, 8];
+
+let sum2 = 0;
+console.log(sum2);
+
+sum2 += marks2[0];  // 0 + 2
+console.log(sum2);
+
+sum2 += marks2[1];  //
+console.log(sum2)
+
+sum2 += marks2[2];  //
+console.log(sum2)
+
+sum2 += marks2[3];  //
+console.log(sum2)
+
