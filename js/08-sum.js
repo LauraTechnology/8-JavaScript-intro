4b9ee97dc6ddc16169cf3c9890dc88a8af847aca
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-21 15:13:23 +0300

    suma, naudojant loop

---
+++ js/08-sum.js
@@ -0,0 +1,10 @@
+// Suma
+//
+// 1.	Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
+// a.	0 - 0
+// b.	0 - 4
+// c.	0 - 100
+// d.	574 - 815
+// e.	-50 - 50
+// f.	-70 - 30
+// 
