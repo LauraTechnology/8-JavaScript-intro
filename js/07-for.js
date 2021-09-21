de538bb91c5a42ae84066f3a8cd10bdea39ae3a5
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-21 14:53:32 +0300

    grammar

--- js/07-for.js
+++ js/07-for.js
@@ -1,6 +1,6 @@
 
 
-// Parasyti cikla, kurima isspaudinsiu skaicius nuo 3 iki 7 imtinai
+// Parasyti cikla, kuriame isspausdinsiu skaicius nuo 3 iki 7 imtinai
 for (let i = 3; i < = 7; i++) {
     console.log(i);
 }
