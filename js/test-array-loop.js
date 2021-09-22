04612567a805c489e9ed3ecb74072a99a6c92921
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-22 14:52:22 +0300

    bandau suprasti array[sarasas/masyvas] ir loop

---
+++ js/test-array-loop.js
@@ -0,0 +1,19 @@
+// sukurti masyva (sarasa/array) ir sukurti jam loop (cikla)
+
+const savaite = ['Prd', 'And', 'Trd', 'Ktd', 'Ptd', 'Štd', 'Skd'];
+
+console.log(savaite);
+console.log(savaite[1]);
+console.log(savaite[5]);
+
+
+// for (let i = 0; i <savaite.length; i++) {
+//     console.log(savaite[i]);
+// }
+
+let reverse = '';
+
+ for (let i = 0; i < savaite.length; i++) {
+     reverse += savaite[savaite.length - 1 - i];
+ }
+ console.log(savaite, '->', reverse);
