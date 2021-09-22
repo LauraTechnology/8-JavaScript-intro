ea8765ff4b9b4bc07f2f88a791986df0056f99ea
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-22 10:58:20 +0300

    commit

--- js/09-reverse.js
+++ js/09-reverse.js
@@ -1,11 +1,42 @@
 // 2.	panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
 // a.	pvz.: “abcdef” -> “fedcba”
 
+/* 1 būdas */
+
 const text = 'abcdef';
 let reverse = '';
+const masyvas = ['vienas', 'du', 3]
+console.log(masyvas[1])
 
-for (let i = 0; i < text.length; i++) {
-    reverse = text[i] + reverse; /* reverse += text [i] */
+// for (let i = 0; i < text.length; i++) {
+//     reverse = text[i] + reverse; /* reverse += text [i] 
+// }
+
+
+
+/* 2 būdas */
+
+// for (let i = 0; i < text.length; i++) {
+//     reverse += text[text.length - 1 - i];
+// }
+// 
+// /* 3 būdas */
+// 
+// for (let i = text.length - 1; i >= 0; i--) {
+//     reverse += text[i];
+// }
+
+/* 4 būdas */
+
+for (let i = text.length; i > 0; i--) {
+    console.log(i)
+    console.log(reverse)
+    reverse += text[i - 1];
 }
 
+
+
 console.log(text, '->', reverse);
+
+
+sukurti masyva (sarasa/array) ir sukurti jam loop (cikla)
