112ff5f89af25a43a29b1d6841c4e8f54b9f73f8
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-22 14:38:25 +0300

    ternary UŽDUOTIS

---
+++ js/05.2-ternary.js
@@ -0,0 +1,19 @@
+// Here is an example of code that could be shortened with the conditional operator:
+
+// var userType;
+// 
+// 
+// 
+// if (userIsYoungerThan18) {
+//   userType = "Minor";
+// } else {
+//   userType = "Adult";
+// }
+// 
+
+ 
+// if (userIsYoungerThan21) {
+//   serveDrink("Grape Juice");
+// } else {
+//   serveDrink("Wine");
+// }
