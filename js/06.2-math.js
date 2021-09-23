320fbaa060c2e1b3816d3813e97b76ecb096c400
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-23 14:41:25 +0300

    r y t a s

--- js/06.2-math.js
+++ js/06.2-math.js
@@ -18,3 +18,38 @@ console.log('sum3:', sum3);
 markIndex3 += 1;
 sum3 += marks3[markIndex3];
 console.log('sum3:', sum3);
+
+console.log('------------');
+
+const abc = ['r', 'y', 't', 'a', 's'];
+
+let zodis = '';
+let abcIndex = 0;
+
+zodis += abc[abcIndex];
+    console.log(zodis);
+
+abcIndex += 1;
+    console.log(abcIndex);
+zodis += abc[abcIndex];
+    console.log(zodis);
+
+abcIndex += 1;
+    console.log(abcIndex);
+zodis += abc[abcIndex];
+    console.log(zodis);
+
+abcIndex += 1;
+    console.log(abcIndex);
+zodis += abc[abcIndex];
+    console.log(zodis);
+
+    abcIndex += 1;
+    console.log(abcIndex);
+zodis += abc[abcIndex];
+    console.log(zodis);
+
+
+
+
+
