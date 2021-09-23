c78c8d00a6848c0dd72c5cac95bee16114a6580a
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-23 11:19:03 +0300

    -

---
+++ js/06.2-math.js
@@ -0,0 +1,17 @@
+const marks3 = [2, 4, 6, 8];
+
+let sum3 = 0;
+let markIndex3 = 0;
+console.log(sum3);
+
+sum3 += marks3[markIndex3];
+console.log('sum3:', sum3);
+
+sum3 += marks3[markIndex3];
+console.log('sum3:', sum3);
+
+sum3 += marks3[markIndex3];
+console.log('sum3:', sum3);
+
+sum3 += marks3[markIndex3];
+console.log('sum3:', sum3);
