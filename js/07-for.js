4aec3dfff092ce76560773faa96e53a80d33d9c6
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-26 16:41:18 +0300

    -

--- js/07-for.js
+++ js/07-for.js
@@ -1,3 +1,24 @@
+/*
+
+FOR LOOP (ciklas):
+
+for () {}
+
+*/
+
+const rytas = ['r', 'y', 't', 'a', 's'];
+
+let zodis = '';
+let rytasIndex = 0;
+zodis += rytas[rytasIndex++];
+zodis += rytas[rytasIndex++];
+zodis += rytas[rytasIndex++];
+zodis += rytas[rytasIndex++];
+zodis += rytas[rytasIndex++];
+
+    console.log(zodis);
+
+console.log('----------');
 
 
 // Parasyti cikla, kurima isspaudinsiu skaicius nuo 3 iki 7 imtinai
@@ -14,4 +35,21 @@ const marks = [5, 7, 10, 4, 2, 10, 8, 9, 1, 3];
 
 for (let i = 0; i < marks.length; i++) {
     console.log(marks[i]);
+}
+
+//Ejimas per sarasa:
+const pazymiai = [ 3, 8, 5, 10, 1, 6, 3, 7];
+
+for (let i = 0; i < marks.length; i++) {
+    console.log(marks[i]);
+}
+
+console.log('-----------');
+
+const abc = ['a', 'b', 'c', 'd'];
+
+for ( let i = 0; i < abc.length; i++) {
+    const raide = abc[i];
+
+    console.log(raide);
 }
