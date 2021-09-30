bb18cf2b982b5590d59dcff2db7371c5bbcc2a56
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-09-30 19:33:50 +0300

    -

--- js/09-reverse.js
+++ js/09-reverse.js
@@ -14,42 +14,43 @@ let reverse = '';
 
 
 
-/* 2 būdas */
-
- for (let i = 0; i < text.length; i++) {
-     reverse += text[text.length - 1 - i];
- }
- 
- /* 3 būdas */
- 
- for (let i = text.length - 1; i >= 0; i--) {
-     reverse += text[i];
- }
-
-/* 4 būdas */
-
-for (let i = text.length; i > 0; i--) {
-    console.log(i)
-    console.log(reverse)
-    reverse += text[i - 1];
-}
-
-
-
-console.log(text, '->', reverse);
-
-console.log('-----------');
-
-const sakinys = 'Labas rytas';
-
-for (let i = 0; i < sakinys.length; i++) {
-    const simbolis = sakinys[i];
-    console.log(simbolis);
-}
-
-for (let i = 0; i < sakinys.length; i++) {
-    console.log(sakinys[i]);
-}
-
-console.log('----------');
-
+///* 2 būdas */
+//
+// for (let i = 0; i < text.length; i++) {
+//     reverse += text[text.length - 1 - i];
+// }
+// 
+// /* 3 būdas */
+// 
+// for (let i = text.length - 1; i >= 0; i--) {
+//     reverse += text[i];
+// }
+//
+///* 4 būdas */
+//
+//for (let i = text.length; i > 0; i--) {
+//    console.log(i)
+//    console.log(reverse)
+//    reverse += text[i - 1];
+//}
+//
+//
+//
+//console.log(text, '->', reverse);
+//
+//console.log('-----------');
+//
+//const sakinys = 'Labas rytas';
+//
+//for (let i = 0; i < sakinys.length; i++) {
+//    const simbolis = sakinys[i];
+//    console.log(simbolis);
+//}
+//
+//for (let i = 0; i < sakinys.length; i++) {
+//    console.log(sakinys[i]);
+//}
+//
+//console.log('----------');
+//
+//
