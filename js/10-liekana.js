aafd80c501add5582534ebb42d85271daf15355d
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-03 20:53:21 +0300

    liekana '%'

---
+++ js/10-liekana.js
@@ -0,0 +1,19 @@
+/*
+Suskaičiuoti, kiek nurodytame intervale yra skaičių, kurie dalijasi be liekanos iš 3, 5 ir 7 atskirai:
+a.	0 - 11
+b.	8 - 31
+c.	-18 - 18
+rezultatą pateikti tokiu formatu:
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra 4 vienetai.
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 vienetai.
+Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai.
+*/
+
+const nuo = 0;
+const iki = 11;
+const daliklis = 3;
+let kiekis = 0;
+
+for (let i = nuo; i <= iki; i++) {
+    console.log(i, i / daliklis);
+}
