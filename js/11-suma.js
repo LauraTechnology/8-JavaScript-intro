936f0c7fdac12edc47a7012eb460c20ea5d97865
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-04 11:24:22 +0300

    Duomenų perdavimas į funkciją

---
+++ js/11-suma.js
@@ -0,0 +1,23 @@
+const a = 8;
+const b = 5;
+
+const c = 99;
+const d = 88;
+
+const e = 54155;
+const f = 89514;
+
+// suma:
+// const s = (a + b) * 10;
+// console.log(s);
+
+function desimteriopa_suma(pirmas, antras) {
+    console.log(pirmas);
+    console.log(antras);
+}
+
+desimteriopa_suma(a, b);
+console.log('--------');
+desimteriopa_suma(c, d);
+console.log('--------');
+desimteriopa_suma(e, f);
