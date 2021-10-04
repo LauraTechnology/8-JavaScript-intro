00da43f2290d85b63a7ec9913446aa760d3fe512
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-04 13:11:12 +0300

    funkcija: pažymių vidurkis

---
+++ js/12-marks.js
@@ -0,0 +1,16 @@
+function pazymiuVidurkis(vardas, pazymiai) {
+    let suma = 0;
+
+    for (let i = 0; i < pazymiai.length; i++) {
+        suma += pazymiai[i];
+    }
+
+    const average = suma / pazymiai.length;
+
+    // Vardenis: pazymiu vidurkis yra 6.
+    console.log(`${vardas}: pazymiu vidurkis yra ${average}`);
+}
+
+pazymiuVidurkis('Petriukas', [10, 2, 8, 4]);
+pazymiuVidurkis('Maryte', [10, 8, 6, 4]);
+pazymiuVidurkis('Alphe', [10, 9, 10, 7, 2, 7]);
