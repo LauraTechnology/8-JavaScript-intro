d74cec7a23382bc6d9d1fe399bfea59c40709370
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-10-04 17:46:02 +0300

    Java Script mini uzduotys - funkcijos 1.

---
+++ js/13-empty.js
@@ -0,0 +1,15 @@
+//	Funkcija pavadinimu “tusciaFunkcija”:
+//	    -nepriima jokių kintamųjų
+//	    -neatlieka jokios vidinės logikos
+//	    -gražina boolean tipo reikšmę “false”
+//	TESTAS:
+//	console.log( tusciaFunkcija() );
+//	rezultatas: false
+
+function tusciaFunkcija() {
+    return false;
+}
+
+
+
+console.log(tusciaFunkcija());
